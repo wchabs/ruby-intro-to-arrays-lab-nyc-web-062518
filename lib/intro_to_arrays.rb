aed92a29a30c7abea95c45243d2a029cd84ed340
William Chabla <w_chabla@hotmail.com>
2018-06-21 17:26:58 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,13 +21,13 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  first(array)
+  array.first()
 end
 
 def last_element_with_array_methods(array)
-  last(array)
+  array.last()
 end
 
 def length_of_an_array(array)
-  array.length
+  array.length()
 end
